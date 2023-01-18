65fba5f80b42b934137ea8fc3039bf2f0633c907
jihyuk <kwlgur0416@gmail.com>
2023-01-18 21:32:04 +0900

    add scrollView and imageView to PageWaveVC

--- ListenApp/Scences/player/PlayerPageView/PageWaveViewController.swift
+++ ListenApp/Scences/player/PlayerPageView/PageWaveViewController.swift
@@ -7,13 +7,50 @@
 
 import UIKit
 
-
 class PageWaveViewController : UIViewController {
     
+    lazy var imageView : UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "MusicBasic")
+        imageView.contentMode = .scaleToFill
+        imageView.frame.size.width = view.frame.width * 3
+        imageView.frame.size.height = view.frame.height / 2
+        
+        return imageView
+    }()
+    
+    lazy var scrollView : UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.alwaysBounceHorizontal = true
+        scrollView.showsHorizontalScrollIndicator = true
+        
+        scrollView.contentSize.width = imageView.frame.size.width
+        
+        scrollView.addSubview(imageView)
+        
+        return scrollView
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        view.backgroundColor = .yellow
+        setLayout()
+
+    }
+}
+
+
+extension PageWaveViewController {
+    func setLayout(){
+        
+        [scrollView].forEach{
+            view.addSubview($0)
+        }
+        
+        scrollView.snp.makeConstraints{
+            $0.top.bottom.leading.trailing.equalToSuperview()
+        }
+        
     }
 }
